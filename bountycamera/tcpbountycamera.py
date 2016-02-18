54024beefc6628f1dd754c9d0925badbcf8a15ff
dwicke <drewwicke@gmail.com>
2016-02-18 09:34:25 -0500

    added a tcp version of the bounty camera that sends whole image

---
+++ bountycamera/tcpbountycamera.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+"""OpenCV feature detectors with ros CompressedImage Topics in python.
+
+This example subscribes to a ros topic containing sensor_msgs 
+CompressedImage. It converts the CompressedImage into a numpy.ndarray, 
+then detects and marks features in that image. It finally displays 
+and publishes the new image - again as CompressedImage topic.
+"""
+__author__ =  'Simon Haller <simon.haller at uibk.ac.at>'
+__version__=  '0.1'
+__license__ = 'BSD'
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+#from scipy.ndimage import filters
+
+
+# Ros libraries
+import roslib
+import rospy
+
+import socket
+
+# Ros Messages
+from sensor_msgs.msg import Image
+
+VERBOSE=False
+
+class image_feature:
+
+    def __init__(self):
+        '''Initialize ros subscriber'''
+  self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  self.sock.connect(('10.112.120.213', 8052))
+  
+  
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/camera/image_raw",
+            Image, self.callback,  queue_size = 1)
+        if VERBOSE :
+            print "subscribed to /camera/image/compressed"
+
+
+    def callback(self, ros_data):
+        '''Callback function of subscribed topic. 
+        Here images get converted and features detected'''
+        if VERBOSE : 
+    print 'received image of size: "%d" x "%d"' % (ros_data.width, ros_data.height)
+  if VERBOSE :
+    print ' len of data = "%d"' %  (len(ros_data.data))
+  self.sock.sendall(ros_data.data)
+  
+
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    ic = image_feature()
+    rospy.init_node('image_feature', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down ROS Image feature detector module"
+
+if __name__ == '__main__':
+    main(sys.argv)
