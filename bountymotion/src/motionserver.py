9c4216e41a9ebe7e6b0df71338ea41e00b197ba6
dwicke <drewwicke@gmail.com>
2016-02-29 17:54:43 -0500

    changed interval

--- bountymotion/src/motionserver.py
+++ bountymotion/src/motionserver.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
                             print "frequency was: %d and the succRate was %f" % (frequency, succCount / recvCount)
                     succCount = 0.0 # reset
                     recvCount = 0.0 # reset
-                    frequency += 2.0
+                    frequency += 5.0
                     curTS = 'tsData_' + str(frequency)
                     print 'frequency = %d' % (frequency)
 
