142d7859069f6aa4b26a8026b1d93ab634028924
dwicke <drewwicke@gmail.com>
2016-03-01 00:38:11 -0500

    blocking zero

--- bountymotion/src/savetime.py
+++ bountymotion/src/savetime.py
@@ -2,7 +2,7 @@ import socket
 import signal
 import time
 import select
-
+import numpy as np
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
@@ -32,6 +32,7 @@ def saveT():
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.setblocking(0)
+sock.settimeout(T)
 sock.bind((UDP_IP, UDP_PORT))
 
 
@@ -39,10 +40,15 @@ sock.bind((UDP_IP, UDP_PORT))
 
 
 while True:
+    sock.settimeout(T)
     tick = time.time()
-    sockRecv,_,_ = select.select([sock], [],[], T)
 
-    data, addr = sockRecv[0].recvfrom(1024) # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    ddt = np.abs(tick - float(data))
+    while(T < ddt):
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        ddt = np.abs(tick - float(data))
+
 
     tock = time.time()
     dt = T - (tock - tick)
