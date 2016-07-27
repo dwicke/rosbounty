72a712697396256be55f4ae77bff71184e52a277
dwicke <drewwicke@gmail.com>
2016-07-27 17:08:18 -0400

    made a script to start all the achd

--- cloudbot_vs/src/cmd.sh
+++ cloudbot_vs/src/cmd.sh
@@ -1,4 +1,15 @@
-achd push 159.203.67.159 15920367159VSTaskImg &
-achd pull 159.203.67.159 15920367159VSResp &
-roslaunch libuvc_camera libuvc_camera.launch &
-rosrun cloudbot_vs CloudVSWithRos.py
+#!/bin/bash
+#achd push 159.203.67.159 15920367159VSTaskImg &
+#achd pull 159.203.67.159 15920367159VSResp &
+#roslaunch libuvc_camera libuvc_camera.launch &
+#rosrun cloudbot_vs CloudVSWithRos.py
+
+
+for i in `cat ipaddresses.txt`
+do
+foo=$i
+achd push $i "${foo//./}"VSTaskImg &
+achd pull $i "${foo//./}"VSResp &
+done
+
+
